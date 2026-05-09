32fc1f7e60c386e734bb19ba6642d7c9047f294c
Roer2608 <rodney.moralesv@gmail.com>
2026-05-08 23:21:28 -0500

    fix conexion pool

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const mysql = require("mysql2");
 
 const app = express();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect(err => {
